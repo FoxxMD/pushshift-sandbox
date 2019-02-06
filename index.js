a4d6a1285508cec2a21e25ccc002835d08376636
FoxxMD <matt.duncan13@gmail.com>
2019-02-05 22:35:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const fetch  = require( 'node-fetch' );
+const moment = require( 'moment' );
+const URLSearchParams = require('url-search-params');
+
+const searchTerm = 'banana';
+const subreddits = [ 'askreddit' ];
+const after      = moment().subtract( 1, 'year' );
+const before     = moment();
+
+const params = new URLSearchParams();
+
+params.append( 'before', before.unix() );
+params.append( 'after', after.unix() );
+params.append( 'subreddits', subreddits.join( ',' ) );
+params.append( 'q', searchTerm );
+
+const url = `https://api.pushshift.io/reddit/comment/search?${params.toString()}`;
+
+const results = fetch( url )
+	.then( res => res.json() )
+	.then( json => console.log(json) );
